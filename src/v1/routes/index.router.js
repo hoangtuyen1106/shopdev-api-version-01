95eff363847272e169afe99eb5226f770968cb95
Tuyen <hoangtuyen1106@gmail.com>
2026-02-22 00:49:32 +0700

    feat: add api users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,24 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success api',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'Tuyen',
+                age: 32
+            },
+            {
+                name: 'Ronado',
+                age: 39
+            },
+            {
+                name: 'Messi',
+                age: 37
+            }
+        ]
+    })
+})
 module.exports = router;
